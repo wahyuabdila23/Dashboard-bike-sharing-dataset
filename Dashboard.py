2cdc7bcdbf37404e50af27fe1de56410c9d48612
mwahyuabdila <116945436+wahyuabdila23@users.noreply.github.com>
2024-05-23 13:25:33 +0700

    Update

--- Dashboard.py
+++ Dashboard.py
@@ -2,67 +2,54 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+sns.set(style='dark')
 
-# Judul untuk aplikasi Streamlit
-st.title("Dashboard Penyewaan Sepeda")
+def create_count_season_df(df)
+    count_season_df = df.groupby("season").cnt.sum().sort_values(ascending=False).reset_index()
+    return count_season_df
 
-# Membaca dataset
-data = pd.read_csv("all_data.csv")
+def create_count_workingday_df(df)
+    count_workingday_df = df.groupby("workingday").cnt.sum().sort_values(ascending=False).reset_index()
+    return count_workingday_df
 
-# Konversi kolom "dteday" menjadi tipe data datetime
-data["dteday"] = pd.to_datetime(data["dteday"])
+cleaned_df = pd.read_csv("all_data.csv")
 
-# Plot jumlah penyewa sepeda per hari
-st.header("Jumlah Penyewa Sepeda per Hari (2011-2012)")
-plt.figure(figsize=(15, 5))
-sns.lineplot(x='dteday', y='cnt', data=data)
-plt.xlabel("Hari")
-plt.ylabel("Jumlah Penyewa")
-plt.title("Jumlah Penyewa Sepeda per Hari (2011-2012)")
-st.pyplot()
+datetime_columns = ["dteday"]
+cleaned_df.sort_values(by="dteday", inplace=True)
+cleaned_df.reset_index(inplace=True)
 
-# Grup berdasarkan tahun
-cnt_year = data.groupby("yr").cnt.sum().sort_values(
-    ascending=False).reset_index()
+for column in datetime_columns:
+    cleaned_df[column] = pd.to_datetime(cleaned_df[column])
 
-# Grup berdasarkan musim
-cnt_season = data.groupby("season").cnt.sum(
-).sort_values(ascending=False).reset_index()
+# Filter data
+min_date = all_df["dteday"].min()
+max_date = all_df["dteday"].max()
 
-# Grup berdasarkan hari kerja
-cnt_holiday = data.groupby("workingday").cnt.sum(
-).sort_values(ascending=False).reset_index()
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
 
-# Plot jumlah penyewa sepeda berdasarkan musim
-st.header("Jumlah Penyewa Sepeda Berdasarkan Musim")
-plt.figure(figsize=(10, 5))
-sns.barplot(y="cnt", x="season", data=cnt_season.sort_values(
-    by="cnt", ascending=False))
-plt.title("Jumlah Penyewa Sepeda Berdasarkan Musim")
-plt.ylabel("Jumlah Penyewa")
-plt.xticks([0, 1, 2, 3], ['Semi', 'Panas', 'Gugur', 'Salju'])
-plt.xlabel("Musim")
-st.pyplot()
+main_df = all_df[(cleaned_df["dteday"] >= str(start_date)) & 
+                (cleaned_df["dteday"] <= str(end_date))]
 
-# Plot jumlah penyewa sepeda berdasarkan hari kerja atau tidak
-st.header("Jumlah Penyewa Sepeda Berdasarkan Hari Libur atau Tidak")
-plt.figure(figsize=(10, 5))
-sns.barplot(y="cnt", x="workingday",
-            data=cnt_holiday.sort_values(by="cnt", ascending=False))
-plt.title("Jumlah Penyewa Sepeda Berdasarkan Hari Libur atau Tidak")
-plt.ylabel("Jumlah Penyewa")
-plt.xticks([0, 1], ['Hari Libur', 'Hari Kerja'])
-plt.xlabel("Hari Libur atau Tidak")
-st.pyplot()
+# st.dataframe(main_df)
 
-# Matriks korelasi
-st.header("Matriks Korelasi")
-selected_columns = ['temp', 'atemp', 'hum', 'windspeed', 'cnt']
-subset_df = data[selected_columns]
-correlation_matrix = subset_df.corr()
+# # Menyiapkan berbagai dataframe
+count_season_df = create_count_season_df(main_df)
+count_workingday_df = count_workingday_df(main_df)
 
-# Plot matriks korelasi dengan seaborn
-plt.figure(figsize=(8, 6))
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
-plt.title('Matriks Korelasi')
-st.pyplot()
+
+# plot number of daily orders (2021)
+st.header('Bike Sharing Dashboard :sparkles:')
+    
+
+# customer demographic
+st.subheader("Customer Demographics")
+
